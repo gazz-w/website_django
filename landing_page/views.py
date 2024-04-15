74981a42a6fe5e65122e1227f7ac7aecad267c3c
Gazz-w <105929513+Gazz-w@users.noreply.github.com>
2024-04-15 17:35:48 -0300

    adicionado nomes dinâmicos dentro dos cards no html

--- landing_page/views.py
+++ landing_page/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'landing_page/index.html')
+
+    dados = {
+        1: {"nome": "Exame de imagem",
+            "descricao": "Ultrassonografia, raio x, tomografia, entre outros."},
+        2: {"nome": "Exame de sangue",
+            "descricao": "todos os tipos de exame de sangue"},
+        3: {"nome": "Consulta médica",
+            "descricao": "diversas especialidades"},
+        4: {"nome": "Nutricionista",
+            "descricao": "Acompanhamento nutricional, nutrição esportiva, etc"},
+        5: {"nome": "Psicologia",
+            "descricao": "Consulta psicológica, Diagnóstico de TDAH, ansiedade, etc"},
+        6: {"nome": "Vacinas",
+            "descricao": "Vacina da gripe, dengue, covid, entre outros"}
+    }
+
+    return render(request, 'landing_page/index.html', {"cards": dados})
 
 
 def imagem(request):
