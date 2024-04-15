ce3ba33d3a4cf576200b832ba29808401cda017b
Gazz-w <105929513+Gazz-w@users.noreply.github.com>
2024-04-15 18:46:01 -0300

    adicionado banco de dados

--- landing_page/models.py
+++ landing_page/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Servico(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    descricao = models.CharField(max_length=250, null=False, blank=False)
+
+    def __str__(self):
+        return f"Serviço [Nome={self.nome}]"
+
+
 # Create your models here.
