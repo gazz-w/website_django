d2b554056752e36fe2848eb5366972eb1f3d5f96
Gazz-w <105929513+Gazz-w@users.noreply.github.com>
2024-04-17 16:42:30 -0300

    imagens dinâmica adicionadas ao card

--- landing_page/models.py
+++ landing_page/models.py
@@ -3,10 +3,30 @@ from django.db import models
 
 class Servico(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
-    descricao = models.CharField(max_length=250, null=False, blank=False)
+    especialidade = models.CharField(
+        max_length=100, null=False, blank=False, default='default')
+    profissionais = models.ManyToManyField(
+        'Profissional', related_name='servicos')
 
     def __str__(self):
-        return f"Serviço [Nome={self.nome}]"
+        return f"Serviço [Nome={self.nome}, Especialidade={self.especialidade}]"
 
+
+class Profissional(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    especialidade = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"Profissional [Nome={self.nome}, Especialidade={self.especialidade}]"
+
+
+class Paciente(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(unique=True)
+    cpf = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return f"Paciente [Nome={self.nome}]"
 
 # Create your models here.
--- landing_page/views.py
+++ landing_page/views.py
@@ -5,17 +5,23 @@ def index(request):
 
     dados = {
         1: {"nome": "Exame de imagem",
-            "descricao": "Ultrassonografia, raio x, tomografia, entre outros."},
+            "descricao": "Ultrassonografia, raio x, tomografia, entre outros.",
+            "imagem": "/assets/imagens/landing_page/exame_imagem.png"},
         2: {"nome": "Exame de sangue",
-            "descricao": "todos os tipos de exame de sangue"},
+            "descricao": "todos os tipos de exame de sangue",
+            "imagem": "/assets/imagens/landing_page/exame_sangue.png"},
         3: {"nome": "Consulta médica",
-            "descricao": "diversas especialidades"},
+            "descricao": "diversas especialidades",
+            "imagem": "/assets/imagens/landing_page/consulta_medica.png"},
         4: {"nome": "Nutricionista",
-            "descricao": "Acompanhamento nutricional, nutrição esportiva, etc"},
+            "descricao": "Acompanhamento nutricional, nutrição esportiva, etc",
+            "imagem": "/assets/imagens/landing_page/nutricao.png"},
         5: {"nome": "Psicologia",
-            "descricao": "Consulta psicológica, Diagnóstico de TDAH, ansiedade, etc"},
+            "descricao": "Consulta psicológica, Diagnóstico de TDAH, ansiedade, etc",
+            "imagem": "/assets/imagens/landing_page/psicologia.png"},
         6: {"nome": "Vacinas",
-            "descricao": "Vacina da gripe, dengue, covid, entre outros"}
+            "descricao": "Vacina da gripe, dengue, covid, entre outros",
+            "imagem": "/assets/imagens/landing_page/vacina.png"}
     }
 
     return render(request, 'landing_page/index.html', {"cards": dados})
